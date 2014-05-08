1c3f71d2f264bcb2d1599fda8bf622c52b05ee9c
tstartrekker <tstartrekker@gmail.com>
2014-05-09 01:40:37 +0600

    Finished layout and rutes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,42 +1,35 @@
 require 'spec_helper'
 
-describe "Static Pages" do
-	let (:main_title) {"Ruby on Rails Tutorial Sample App"}
+describe "Static pages" do
 
-	describe "Home page" do
-		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
-			expect(page).to have_content ('Sample App')
-		end
+  subject { page }
 
-		it "should have the tilte 'Home'" do
-			visit '/static_pages/home' 
-			expect(page).to have_title ("#{main_title} | Home")
-		end
-	end
+  describe "Home page" do
+    before { visit root_path }
 
-	describe "Help page" do
-		it "should have the content 'Sample App'" do
-			visit '/static_pages/help'
-			expect(page).to have_content ('Help')
-		end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+  end
 
-		it "should have the tilte 'Help'" do
-			visit '/static_pages/help' 
-			expect(page).to have_title ("#{main_title} | Help")
-		end
-	end
+  describe "Help page" do
+    before { visit help_path }
 
-	describe "Contact page" do
-		it "should have the content 'Sample App'" do
-			visit '/static_pages/contact'
-			expect(page).to have_content ('Contact')
-		end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
 
-		it "should have the title 'Contact'" do
-			visit '/static_pages/contact'
-			expect(page).to have_title ("#{main_title} | Contact")
-		end
-	end
+  describe "About page" do
+    before { visit about_path }
 
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
 end
