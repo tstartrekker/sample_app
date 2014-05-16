050776b686b8b53a7174f9b6e1e0d7830f60979f
tstartrekker <tstartrekker@gmail.com>
2014-05-16 23:46:52 +0600

    Done with chapter 8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -30,7 +31,7 @@ describe User do
   end
 
   describe "when email format is invalid" do
-	it "should be invalid" do
+	 it "should be invalid" do
 		addresses = %w[user@foo,com user_at_foo.org example.user@foo foo@bar_baz.com foo@bar+baz.com]
 		addresses.each do |invalid_address|
 			@user.email = invalid_address
@@ -87,5 +88,10 @@ describe User do
 		it { should_not eq user_for_invalid_password }
 		specify { expect(user_for_invalid_password).to be_false }
 	end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
